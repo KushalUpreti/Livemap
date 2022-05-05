c31da8e411958b855dc729dfbddd1a9aa10b8d40
KushalUpreti <55836966+KushalUpreti@users.noreply.github.com>
2022-05-05 17:14:06 +0545

    Lifted sidebar states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Map from "./components/Map";
 import Sidebar from "./components/Sidebar";
 import classes from "./styles/App.module.css";
@@ -7,15 +7,35 @@ import data from "./utils/map2.json";
 function App() {
   const [geoData, setGeoData] = useState(data);
   const [paginationData, setPaginationData] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loadMoreVisible, setLoadMoreVisible] = useState(true);
+  const page_size = 3;
+
+  useEffect(() => {
+    setPaginationData((prev) => {
+      let array = [...prev];
+      let newData = paginate(geoData.features, 3, pageNumber);
+      return [...array, ...newData];
+    });
+    setLoadMoreVisible(geoData.features.length - pageNumber * page_size > 0);
+  }, [pageNumber]);
+
+  function paginate(array, page_size, page_number) {
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+  }
+
+  function loadMore() {
+    setPageNumber((prev) => ++prev);
+  }
 
   return (
     <main className={classes.app}>
       <Map geoData={geoData} paginationData={paginationData} />
       <Sidebar
         geoData={geoData}
-        page_size={3}
         paginationData={paginationData}
-        setPaginationData={setPaginationData}
+        loadMoreVisible={loadMoreVisible}
+        loadMore={loadMore}
       />
     </main>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,32 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import classes from "../styles/Sidebar.module.css";
 import DataItem from "../components/DataItem";
 
-export default function Sidebar({
-  geoData,
-  page_size,
-  paginationData,
-  setPaginationData,
-}) {
-  const [pageNumber, setPageNumber] = useState(1);
-  const [loadMoreVisible, setLoadMoreVisible] = useState(true);
-
-  useEffect(() => {
-    setPaginationData((prev) => {
-      let array = [...prev];
-      let newData = paginate(geoData.features, 3, pageNumber);
-      return [...array, ...newData];
-    });
-    setLoadMoreVisible(geoData.features.length - pageNumber * page_size > 0);
-  }, [pageNumber]);
-
-  function paginate(array, page_size, page_number) {
-    return array.slice((page_number - 1) * page_size, page_number * page_size);
-  }
-
-  function loadMore() {
-    setPageNumber((prev) => ++prev);
-  }
-
+export default function Sidebar({ paginationData, loadMore, loadMoreVisible }) {
   return (
     <aside className={classes.sidebar}>
       <div className={classes.data_wrapper}>
