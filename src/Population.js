6393e1e5dc9a52bb48962e82429cd40248e5b3c6
KushalUpreti <55836966+KushalUpreti@users.noreply.github.com>
2022-05-24 11:57:05 +0545

    Added function to create expression

--- src/Population.js
+++ src/Population.js
@@ -23,6 +23,27 @@ export default function App() {
       zoom: zoom,
     });
 
+    const layers = [
+      "0-1000000",
+      "1000000-2000000",
+      "2000000-3000000",
+      "3000000-4000000",
+      "4000000-5000000",
+      "5000000-6000000",
+      "6000000-7000000",
+      "7000000+",
+    ];
+    const colors = [
+      "#FFEDA0",
+      "#FED976",
+      "#FEB24C",
+      "#FD8D3C",
+      "#FC4E2A",
+      "#E31A1C",
+      "#BD0026",
+      "#800026",
+    ];
+
     map.current.addControl(new mapboxgl.FullscreenControl());
     map.current.addControl(new mapboxgl.NavigationControl());
 
@@ -50,102 +71,16 @@ export default function App() {
         source: "country",
         layout: {},
         paint: {
-          "fill-color": [
-            "case",
-            [
-              "boolean",
-              [
-                "all",
-                [">", ["feature-state", "population"], 0],
-                ["<", ["feature-state", "population"], 100000],
-              ],
-              true,
-            ],
-            "#FFEDA0",
-            [
-              "boolean",
-              [
-                "all",
-                [">", ["feature-state", "population"], 100000],
-                ["<", ["feature-state", "population"], 2000000],
-              ],
-              true,
-            ],
-            "#FED976",
-            [
-              "boolean",
-              [
-                "all",
-                [">", ["feature-state", "population"], 200000],
-                ["<", ["feature-state", "population"], 3000000],
-              ],
-              true,
-            ],
-            "#FEB24C",
-            [
-              "boolean",
-              [
-                "all",
-                [">", ["feature-state", "population"], 300000],
-                ["<", ["feature-state", "population"], 4000000],
-              ],
-              true,
-            ],
-            "#FD8D3C",
-            [
-              "boolean",
-              [
-                "all",
-                [">", ["feature-state", "population"], 400000],
-                ["<", ["feature-state", "population"], 5000000],
-              ],
-              true,
-            ],
-            "#E31A1C",
-            [
-              "boolean",
-              [
-                "all",
-                [">", ["feature-state", "population"], 500000],
-                ["<", ["feature-state", "population"], 6000000],
-              ],
-              true,
-            ],
-            "#BD0026",
-            "#800026",
-          ],
-          // "fill-opacity": 0.9,
+          "fill-color": generatePopulationExpression(layers, colors),
         },
       });
 
       population.forEach((item) => {
-        console.log(item.population < 3500000);
         map.current.setFeatureState(
           { source: "country", id: +item.ADM1_EN },
           { population: item.population }
         );
       });
-
-      const layers = [
-        "0-100000",
-        "100000-200000",
-        "200000-300000",
-        "300000-400000",
-        "400000-500000",
-        "500000-600000",
-        "600000-700000",
-        "700000+",
-      ];
-      const colors = [
-        "#FFEDA0",
-        "#FED976",
-        "#FEB24C",
-        "#FD8D3C",
-        "#FC4E2A",
-        "#E31A1C",
-        "#BD0026",
-        "#800026",
-      ];
 
       // create legend
       const legend = document.getElementById("legend");
@@ -165,6 +100,30 @@ export default function App() {
       });
     });
   }, []);
+
+  function generatePopulationExpression(range, colors) {
+    if (range.length !== colors.length) {
+      return [];
+    }
+    let expression = ["case"];
+    for (let i = 0; i < range.length - 1; i++) {
+      const currentRange = range[i].split("-");
+      let curr = [
+        "boolean",
+        [
+          "all",
+          [">", ["feature-state", "population"], +currentRange[0]],
+          ["<", ["feature-state", "population"], +currentRange[1]],
+        ],
+        true,
+      ];
+      expression.push(curr);
+      expression.push(colors[i]);
+    }
+    expression.push(colors[colors.length - 1]);
+    return expression;
+  }
+
   return (
     <div>
       <div ref={mapContainer} className="map-container" />
