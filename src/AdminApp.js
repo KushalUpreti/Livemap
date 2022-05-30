fc66bc5e8d2e8776b1a36133a21ed84fd24271db
KushalUpreti <55836966+KushalUpreti@users.noreply.github.com>
2022-05-30 13:00:50 +0545

    Added save and cancel buttons

--- src/AdminApp.js
+++ src/AdminApp.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./styles/AdminApp.css";
 
 export default function App() {
@@ -20,32 +20,11 @@ export default function App() {
     setCreateText("");
   }
 
-  function updateKey(key, updatedKey) {
+  function saveObject(updatedObj) {
     setObjects((prev) => {
-      let objects = { ...prev };
-      const value = objects[selectedObject][key];
-      delete objects[selectedObject][key];
-      objects[selectedObject][updatedKey] = value;
-      return objects;
-    });
-  }
-
-  function updateValue(key, updatedValue) {
-    setObjects((prev) => {
-      let objects = { ...prev };
-      objects[selectedObject][key] = updatedValue;
-      return objects;
-    });
-  }
-
-  function addProperty() {
-    setObjects((prev) => {
-      let objects = { ...prev };
-      if (objects[selectedObject][""]) {
-        return objects;
-      }
-      objects[selectedObject][""] = "";
-      return objects;
+      let obj = { ...prev };
+      obj[selectedObject] = updatedObj;
+      return obj;
     });
   }
 
@@ -69,45 +48,14 @@ export default function App() {
         </div>
       </div>
 
-      <div className="edit">
-        <div className="edit__create">
-          <h3>Create new country object</h3>
-          <form onSubmit={createObject}>
-            <input
-              type="text"
-              value={createText}
-              onChange={(e) => {
-                setCreateText(e.target.value);
-              }}
-            />
-            <input type="submit" value="Create" />
-          </form>
-        </div>
-
-        {selectedObject && (
-          <div>
-            <h3>{selectedObject}</h3>
-            <p>Add or edit properties</p>
-            <div>
-              {Object.keys(objects[selectedObject]).map((key) => {
-                return (
-                  <PropertyForm
-                    key={key}
-                    keyText={key}
-                    valueText={objects[selectedObject][key]}
-                    updateKey={updateKey}
-                    updateValue={updateValue}
-                  />
-                );
-              })}
-
-              <button className="save" onClick={addProperty}>
-                Add Property
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
+      <ObjectEditor
+        createObject={createObject}
+        createText={createText}
+        objects={objects}
+        selectedObject={selectedObject}
+        setCreateText={setCreateText}
+        saveObject={saveObject}
+      />
     </main>
   );
 }
@@ -141,5 +89,111 @@ function PropertyForm({ keyText, valueText, updateKey, updateValue }) {
         }}
       />
     </form>
+  );
+}
+
+function ObjectEditor({
+  createObject,
+  createText,
+  selectedObject,
+  objects,
+  saveObject,
+  setCreateText,
+}) {
+  const [tempObject, setTempObject] = useState(objects[selectedObject]);
+
+  function cancel() {
+    setTempObject(objects[selectedObject]);
+  }
+
+  function addProperty() {
+    setTempObject((prev) => {
+      let objects = { ...prev };
+      if (objects[""]) {
+        return objects;
+      }
+      objects[""] = "";
+      return objects;
+    });
+  }
+
+  function updateKey(key, updatedKey) {
+    setTempObject((prev) => {
+      const obj = { ...prev };
+      const value = obj[key];
+      delete obj[key];
+      obj[updatedKey] = value;
+      return obj;
+    });
+  }
+
+  function updateValue(key, updatedValue) {
+    setTempObject((prev) => {
+      const obj = { ...prev };
+      obj[key] = updatedValue;
+      return obj;
+    });
+  }
+
+  useEffect(() => {
+    if (!selectedObject) {
+      return;
+    }
+    setTempObject(objects[selectedObject]);
+  }, [selectedObject]);
+
+  return (
+    <div className="edit">
+      <div className="edit__create">
+        <h3>Create new country object</h3>
+        <form onSubmit={createObject}>
+          <input
+            type="text"
+            value={createText}
+            onChange={(e) => {
+              setCreateText(e.target.value);
+            }}
+          />
+          <input type="submit" value="Create" />
+        </form>
+      </div>
+
+      {selectedObject && (
+        <div>
+          <h3>{selectedObject}</h3>
+          <p>Add or edit properties</p>
+          <div>
+            {tempObject &&
+              Object.keys(tempObject).map((key) => {
+                return (
+                  <PropertyForm
+                    key={key}
+                    keyText={key}
+                    valueText={tempObject[key]}
+                    updateKey={updateKey}
+                    updateValue={updateValue}
+                  />
+                );
+              })}
+
+            <button className="action-button" onClick={addProperty}>
+              Add Property
+            </button>
+
+            <button
+              className="action-button save"
+              onClick={() => {
+                saveObject(tempObject);
+              }}
+            >
+              Save
+            </button>
+            <button className="action-button cancel" onClick={cancel}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
